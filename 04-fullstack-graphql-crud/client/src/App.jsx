a8e570a8a9d41eb1d79d70d8266e7cee0ef18cc8
LinAung-Thet <laungthet@gmail.com>
2025-01-19 23:47:15 +1300

    Added useQuery hook

--- 04-fullstack-graphql-crud/client/src/App.jsx
+++ 04-fullstack-graphql-crud/client/src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {useQuery, useMutation, gql} from "@apollo/client";
+
+const GET_USER = gql`
+query GetUsers{
+  getUsers{
+    id
+    age
+    name
+    isMarried
+  }
+}
+`;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const {data, error, loading} = useQuery(GET_USER);
+
+  if(loading) return <p>Loading...</p>;
+
+  if(error) return <p>Error: {error.message}</p>;
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h1>Users</h1>
+    <div>
+      {" "}
+      {data.getUsers.map(user => (
+        <div>
+          <p>Name: {user.name}</p>
+          <p>Age: {user.age}</p>
+          <p>Is this user married: {user.isMarried ? "Yes" : "No"}</p>
+        </div>
+      ))} {" "}
+    </div>
     </>
-  )
+    )
 }
 
 export default App
