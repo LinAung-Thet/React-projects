7d33609008dc95098c3e8d0cd1949cfdef952312
LinAung-Thet <laungthet@gmail.com>
2025-01-20 23:58:16 +1300

    Added query and mutation hooks

--- 04-fullstack-graphql-crud/client/src/App.jsx
+++ 04-fullstack-graphql-crud/client/src/App.jsx
@@ -12,26 +12,71 @@ query GetUsers{
 }
 `;
 
+const GET_USER_BY_ID = gql`
+query GetUsersByID($id: ID!){
+  getUsersById(id: $id){
+    id
+    age
+    name
+    isMarried
+  }
+}
+`;
+
+const CREATE_USER = gql`
+  mutation CreateUser($name: String!, $age: Int!, $isMarried: Boolean){
+  createUser(name: $name, Age: $age, isMarried: $isMarried) {
+    name
+    isMarried
+  }
+}
+`;
+
 function App() {
-  const {data, error, loading} = useQuery(GET_USER);
+  const {
+    data: getUsersData, 
+    error: getUsersError, 
+    loading: getUsersLoading
+  } = useQuery(GET_USER);
 
-  if(loading) return <p>Loading...</p>;
+  const {data: getUsersByIdData, 
+    error: getUsersByIdError, 
+    loading: getUsersByIdLoading
+  } = useQuery(GET_USER_BY_ID, {
+    variables: {id: "2"}
+  });
 
-  if(error) return <p>Error: {error.message}</p>;
+  const [createUser] = useMutation(CREATE_USER)
+  
+  if(getUsersLoading) return <p>Loading...</p>;
+
+  if(getUsersError) return <p>Error: {getUsersError.message}</p>;
 
   return (
     <>
-    <h1>Users</h1>
-    <div>
-      {" "}
-      {data.getUsers.map(user => (
-        <div>
-          <p>Name: {user.name}</p>
-          <p>Age: {user.age}</p>
-          <p>Is this user married: {user.isMarried ? "Yes" : "No"}</p>
-        </div>
-      ))} {" "}
-    </div>
+      <h1>Users</h1>
+
+      <div>
+        {getUsersByIdLoading ? (
+          <p>Loading user ...</p>
+        ) : (
+          <>
+            <h1>Chosen User</h1>
+            <p>{getUsersByIdData.getUsersById.name}</p>
+            <p>{getUsersByIdData.getUsersById.age}</p>
+          </>
+        )}
+      </div>
+      <div>
+        {" "}
+        {getUsersData.getUsers.map(user => (
+          <div>
+            <p>Name: {user.name}</p>
+            <p>Age: {user.age}</p>
+            <p>Is this user married: {user.isMarried ? "Yes" : "No"}</p>
+          </div>
+        ))} {" "}
+      </div>
     </>
     )
 }
